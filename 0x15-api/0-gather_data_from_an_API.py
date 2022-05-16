3f1e2d2d2f96bedeb7a31b1bb79e8a04e90196a6
Anna78990 <takasuanna1990@gmail.com>
2022-05-16 14:12:56 +0000

    pep8 et docc

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a Python script that, using this REST API, for a given employee ID, """
+"""Write a Python script that, using this REST API, for a given employee ID"""
 import json
 import requests
 import sys
