45314ad9aebb3dba75d4da17bf4565d85cc50413
Anna78990 <takasuanna1990@gmail.com>
2022-05-16 13:21:42 +0000

    tout fini

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+   Write a Python script that, using this REST API, for a given employee ID,
+   and extend the Python script to export data in the JSON format.
+"""
+import json
+import requests
+import sys
+import json
+
+args = sys.argv
+n = args[1]
+
+users = requests.get('https://jsonplaceholder.typicode.com/users')
+tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+uj = users.json()
+tj = tasks.json()
+id = uj[int(n)-1]['id']
+person = uj[int(n)-1]['username']
+
+d_value = []
+for i in tj:
+    if i['userId'] == int(n):
+        d_value.append({"task": i['title'], "completed": i['completed'],
+                       "username": person})
+j = {n: d_value}
+json_file = open('{}.json'.format(n), 'w')
+json.dump(j, json_file)
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+   Write a Python script that, using this REST API, for a given employee ID,
+   and extend the Python script to export data in the JSON format.
+"""
+import json
+import requests
+import json
+
+users = requests.get('https://jsonplaceholder.typicode.com/users')
+tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+uj = users.json()
+tj = tasks.json()
+
+len_u = len(uj)
+d = {}
+for u in range(0, len_u):
+    d_value = []
+    for i in tj:
+        if i['userId'] == uj[u]['id']:
+            d_value.append({"username": uj[u]['username'], "task": i['title'],
+                           "completed": i['completed']})
+    d[uj[u]['id']] = d_value
+json_file = open('todo_all_employees.json', 'w')
+json.dump(d, json_file)
