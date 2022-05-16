263425a4aa7d2bb8b9740136fb557ec9f4904c88
Anna78990 <takasuanna1990@gmail.com>
2022-05-16 14:25:34 +0000

    pep8 et docc pour 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,21 +5,23 @@
 """
 import json
 import requests
-import json
 
-users = requests.get('https://jsonplaceholder.typicode.com/users')
-tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
-uj = users.json()
-tj = tasks.json()
 
-len_u = len(uj)
-d = {}
-for u in range(0, len_u):
-    d_value = []
-    for i in tj:
-        if i['userId'] == uj[u]['id']:
-            d_value.append({"username": uj[u]['username'], "task": i['title'],
-                           "completed": i['completed']})
-    d[uj[u]['id']] = d_value
-json_file = open('todo_all_employees.json', 'w')
-json.dump(d, json_file)
+if __name__ == "__main__":
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    uj = users.json()
+    tj = tasks.json()
+
+    len_u = len(uj)
+    d = {}
+    for u in range(0, len_u):
+        d_value = []
+        for i in tj:
+            if i['userId'] == uj[u]['id']:
+                d_value.append({"username": uj[u]['username'],
+                                "task": i['title'],
+                                "completed": i['completed']})
+        d[uj[u]['id']] = d_value
+    json_file = open('todo_all_employees.json', 'w')
+    json.dump(d, json_file)
