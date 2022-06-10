9f7a68bad49895311ccb96e5d21a6f4dbe02fc23
Anna78990 <takasuanna1990@gmail.com>
2022-06-10 13:17:38 +0000

    path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # change the ulimit default value to fix the requests failed
 
 exec { 'delete ulimit':
-    path    => ['/usr/local/bin/', '/bin/'],
-    command => 'sed -i "s/15/4096/" /etc/default/nginx'
+    path    => '/usr/local/bin/:/bin/',
+    command => 'sed -i "5d" /etc/default/nginx'
 }
 
 exec { 'restart ngnix':
-    path    => ['/usr/local/bin/', '/bin/', '/usr/bin/'],
+    path    => ['/usr/local/bin/:/bin/:/usr/bin/'],
     command => 'sudo service nginx restart'
 }
