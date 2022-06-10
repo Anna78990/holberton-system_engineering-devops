f092e2be9a9defece5d08df8e3c0225c3677f8d0
Anna78990 <takasuanna1990@gmail.com>
2022-06-10 11:58:42 +0000

    replace

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # change the ulimit default value to fix the requests failed
 
 exec { 'fix-nginx':
-  command => "sed -i '5d' etc/default/nginx; sudo service nginx restart",
+  command => "sed -i 's/15/2000/g' etc/default/nginx; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
