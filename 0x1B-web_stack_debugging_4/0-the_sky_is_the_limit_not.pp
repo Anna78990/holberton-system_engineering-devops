35a7af4e7d5bd6893676d7f4b02388da32aac857
Anna78990 <takasuanna1990@gmail.com>
2022-06-11 21:48:26 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # change the ulimit default value to fix the requests failed
 
-exec { 'delete ulimit':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => 'sed -i "s/15/4096/" /etc/default/nginx'
+exec {'Change Ulimit':
+path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+command => 'sed -i "s/ULIMIT=.*/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
 }
 
-exec { 'restart ngnix':
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-    command => 'sudo service nginx restart'
+exec {'Restart nginx':
+path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+command => 'sudo service nginx restart',
 }
